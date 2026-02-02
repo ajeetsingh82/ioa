7e4676f4748e835814405de18f34d102c604d186
Ajeet Singh <ajeet.singh82@gmail.com>
2026-02-02 22:10:09 +0530

    [refactor] draft

--- src/prompts.py
+++
@@ -1,9 +0,0 @@
-def get_prompt(role, context, goal):
-    return f"""### ROLE: {role}
-### CONTEXT:
-{context}
-
-### TASK: {goal}
-### REQUIREMENT: Answer in plain English. No other languages.
-
-### RESPONSE:"""
